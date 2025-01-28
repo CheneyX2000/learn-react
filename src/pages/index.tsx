06359e08634799419eea8cf1e93f23a109d5eddc
CheneySheu <xuchengyou728@gmail.com>
2025-01-28 14:47:00 -0500

    commit to get pull from sync

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,10 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
-import RecipeList from './qcomps/recipes'
+import Counter from './qcomps/snapshot'
 
 export default function Home() {
   return (
-    <RecipeList />
+    <Counter />
   )
 }
